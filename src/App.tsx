4af6c9f4606e361972e69b30eadf376da8a15504
minakshee <minakshee.tekade@gmail.com>
2026-04-15 16:39:27 -0400

    search-filter changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 //import Counter from "./components/Counter/Counter";
 
-import TodoList from "./components/TodoList/TodoList";
+import SearchFilter from "./components/SearchFilter/Searchfilter";
+//import TodoList from "./components/TodoList/TodoList";
 
 export default function App() {
   //return <Counter />;
-  return <TodoList />;
+  //return <TodoList />;
+  return <SearchFilter />;
 }
